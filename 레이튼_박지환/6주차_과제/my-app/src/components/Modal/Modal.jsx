904bea4785e144765eeb8a83f1ee53479742efd1
zihwanpack <jihwan8807@naver.com>
2023-11-03 11:52:31 +0900

    CHORE: modal 수정 완료

--- 레이튼_박지환/6주차_과제/my-app/src/components/Modal/Modal.jsx
+++ 레이튼_박지환/6주차_과제/my-app/src/components/Modal/Modal.jsx
@@ -7,10 +7,12 @@ export default function Modal() {
   const isModal = useSelector((state) => state.modal.isModal);
   const dispatch = useDispatch();
   return (
-    <S.Modal style={{ display: isModal ? "block" : "none" }}>
-      <h1>정말 모든 상품을 삭제할까요?</h1>
-      <button onClick={() => dispatch(clearCart())}>예</button>
-      <button onClick={() => dispatch(hideModal())}>아니오</button>
-    </S.Modal>
+    <S.ModalBackground isModal={isModal}>
+      <S.Modal isModal={isModal}>
+        <h1>정말 모든 상품을 삭제할까요?</h1>
+        <button onClick={() => dispatch(clearCart())}>예</button>
+        <button onClick={() => dispatch(hideModal())}>아니오</button>
+      </S.Modal>
+    </S.ModalBackground>
   );
 }
--- 레이튼_박지환/6주차_과제/my-app/src/components/Modal/Modal.styled.jsx
+++ 레이튼_박지환/6주차_과제/my-app/src/components/Modal/Modal.styled.jsx
@@ -1,6 +1,18 @@
 import styled from "styled-components";
 
+export const ModalBackground = styled.div`
+  display: ${(props) => (props.isModal ? "block" : "none")};
+  // props 받아와서 동적으로  스타일링하기
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.7);
+`;
+
 export const Modal = styled.div`
+  display: ${(props) => (props.isModal ? "block" : "none")};
   width: 200px;
   height: 200px;
   background: skyblue;
