c6962d956380a56a1b4f5a5192a756c0df8e7ae4
daindaind <https://github.com/daindaind>
2023-11-03 18:04:33 +0900

    button style 변경

--- 다라_차다인/6주차 과제/src/components/button/Button.styled.js
+++ 다라_차다인/6주차 과제/src/components/button/Button.styled.js
@@ -4,7 +4,6 @@ import theme from "../../theme";
 export const Button = styled.button`
     ${theme.styles.flexCenterColumn};
     padding: 5px 10px;
-    text-align: center;
     background-color: ${theme.color.white};
     color: ${theme.color.button};
     border: 1px solid ${theme.color.button};
