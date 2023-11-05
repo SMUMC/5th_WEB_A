b0e2c871e71a5cc9e920d736fbfcd5c4d5832eb2
daindaind <https://github.com/daindaind>
2023-11-05 17:58:55 +0900

    지환님/gitignore 삭제

--- 레이튼_박지환/4주차_과제/my-app/src/Components/Pages/Popular.jsx
+++
@@ -1,9 +0,0 @@
-import { FetchData } from "../Hooks/FetchData";
-import MovieList from "../MovieList/MovieList";
-import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
-
-export default function Popular() {
-  const { isData, isLoading } = FetchData("popular");
-
-  return <>{isLoading ? <LoadingSpinner /> : <MovieList films={isData} />}</>;
-}
