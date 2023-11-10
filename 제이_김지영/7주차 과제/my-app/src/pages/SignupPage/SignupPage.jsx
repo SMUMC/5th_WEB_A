29cdd710149fe161b2972131912895ca6bef6113
ZJiyoung <kimdanchuchu@gmail.com>
2023-11-11 01:00:23 +0900

    CHORE

--- 제이_김지영/7주차 과제/my-app/src/pages/SignupPage/SignupPage.jsx
+++ 제이_김지영/7주차 과제/my-app/src/pages/SignupPage/SignupPage.jsx
@@ -1,208 +1,137 @@
-import React, { useState } from "react";
 import * as S from "./SignupPage.styled";
+import { useState, useEffect } from "react";
 
-function SignUp() {
-  const [userName, setUserName] = useState("");
-  const [userAge, setUserAge] = useState("");
+export default function SignUp() {
+  const nameRegex = /^[a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣]+$/;
+  const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[\W_]).+$/;
+
+  const [isName, setIsName] = useState(false);
+  const [isEmail, setIsEmail] = useState(false);
+  const [isAge, setIsAge] = useState(false);
+  const [isPassword, setIsPassword] = useState(false);
+  const [isCheckPassword, setIsCheckPassword] = useState(false);
+
+  const [nameMessage, setNameMessage] = useState("");
+  const [emailMessage, setEmailMessage] = useState("");
+  const [ageMessage, setAgeMessage] = useState("");
+  const [passwordMessage, setPasswordMessage] = useState("");
+  const [checkPasswordMessage, setCheckPasswordMessage] = useState("");
+
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
+  const [age, setAge] = useState("");
   const [password, setPassword] = useState("");
-  const [passwordCheck, setPasswordCheck] = useState("");
-  const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const [checkPassword, setCheckPassword] = useState("");
 
-  const isUserName = (value) => {
-    return value.length >= 1;
-  };
-
-  const isUserAge = (value) => {
-    value = parseInt(value);
-    return value >= 19 && Number.isInteger(value);
-  };
-
-  const isEmailForm = (value) => {
-    return value.includes("@");
-  };
-
-  const isPasswordRule = (value) => {
-    const regex = /^(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{6,}$/;
-    return regex.test(value);
-  };
-
-  const isPasswordMatch = (value1) => {
-    return value1 === password;
-  };
-
-  const handleInputChange = (event) => {
-    const { id, value } = event.target;
-
-    switch (id) {
-      case "name":
-        setUserName(value);
-        break;
-      case "age":
-        setUserAge(value);
-        break;
-      case "email":
-        setEmail(value);
-        break;
-      case "pw":
-        setPassword(value);
-        break;
-      case "pw-check":
-        setPasswordCheck(value);
-        break;
-      default:
-        break;
+  const validateField = (value, regex, setMessage, successCheck) => {
+    if (value === "") {
+      setMessage("반드시 입력해주세요!");
+    } else if (!regex.test(value)) {
+      setMessage("형식에 맞게 입력해주세요!");
+    } else {
+      setMessage(" ");
+      successCheck(true);
     }
   };
 
-  // 여기에서 버튼 활성화 로직 추가
-  useState(() => {
-    const check =
-      isUserName(userName) &&
-      isUserAge(userAge) &&
-      isEmailForm(email) &&
-      isPasswordRule(password) &&
-      isPasswordMatch(passwordCheck);
+  useEffect(() => {
+    validateField(name, nameRegex, setNameMessage, setIsName);
+  }, [name]);
 
-    setIsButtonDisabled(!check); //check이 true이면 버튼 활성화
-  }, [userName, userAge, email, password, passwordCheck]);
+  useEffect(() => {
+    validateField(email, emailRegex, setEmailMessage, setIsEmail);
+  }, [email]);
 
-  const handleSubmit = () => {
-    console.log("button clicked!");
-    setTimeout(function () {
-      alert("가입이 완료되었습니다.");
-    }, 0);
+  useEffect(() => {
+    if (age === "" || isNaN(age)) {
+      // 숫자 판별
+      setAgeMessage("반드시 입력해주세요!");
+    } else if (age < 0) {
+      setAgeMessage("음수가 될 수 없습니다!");
+    } else if (age % 1 !== 0) {
+      setAgeMessage("소수가 될 수 없습니다!");
+    } else if (age < 19) {
+      setAgeMessage("19살 이상만 가능합니다!");
+    } else {
+      setAgeMessage(" ");
+      setIsAge(true);
+    }
+  }, [age]);
+
+  useEffect(() => {
+    if (password === "") {
+      setPasswordMessage("반드시 비밀번호를 입력해주세요!");
+    } else if (password.length < 4) {
+      setPasswordMessage("비밀번호는 최소한 4자리 이상이어야합니다.");
+    } else if (password.length > 12) {
+      setPasswordMessage("비밀번호는 최대 12자리까지 입력가능합니다.");
+    } else if (passwordRegex.test(password) === false) {
+      setPasswordMessage("비밀번호 형식에 알맞게 입력해주세요.");
+    } else {
+      setPasswordMessage(" ");
+      setIsPassword(true);
+    }
+  }, [password]);
+
+  useEffect(() => {
+    if (checkPassword !== password || checkPassword === "") {
+      setCheckPasswordMessage("작성한 비밀번호와 동일하지 않습니다!");
+    } else if (checkPassword === password) {
+      setCheckPasswordMessage(" ");
+      setIsCheckPassword(true);
+    }
+  }, [checkPassword]);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    if (isName && isEmail && isAge && isPassword && isCheckPassword) {
+      alert("회원가입에 성공했습니다!");
+      window.location.href = "http://localhost:3000/";
+      console.log();
+    } else {
+      console.log("");
+    }
   };
 
   return (
     <S.Container>
       <div className="login-container">
-        <h1 className="box-title">회원 가입</h1>
-        <hr className="hr" />
-        <form action="" className="login-form">
-          <p style={{ marginBottom: "18px" }}>
-            <label
-              htmlFor="name"
-              style={{ fontSize: "12px" }}
-              className="label"
-            ></label>
-            <input
-              type="text"
-              placeholder="이름"
-              id="name"
-              value={userName}
-              onChange={handleInputChange}
-            />
-          </p>
-          <p style={{ marginBottom: "18px" }}>
-            <label
-              htmlFor="email"
-              style={{ fontSize: "12px" }}
-              className="label"
-            ></label>
-            <input
-              type="text"
-              placeholder="이메일"
-              id="email"
-              value={email}
-              onChange={handleInputChange}
-            />
-          </p>
-          <p>
-            <label
-              htmlFor="age"
-              style={{ fontSize: "12px" }}
-              className="label"
-            ></label>
-            <input
-              type="number"
-              placeholder="나이"
-              id="age"
-              value={userAge}
-              onChange={handleInputChange}
-            />
-            <label
-              style={{
-                color: "red",
-                marginTop: "2px",
-                marginLeft: "8px",
-                fontSize: "12px",
-              }}
-            >
-              {userAge !== "" && userAge < 0
-                ? "나이는 0살 이상이어야 합니다."
-                : ""}
-              {userAge !== "" && userAge >= 0 && userAge < 19
-                ? "나이는 19살 이상이어야 합니다."
-                : ""}
-              {userAge !== "" && !Number.isInteger(parseInt(userAge))
-                ? "정수를 입력하세요."
-                : ""}
-            </label>
-          </p>
-          <p>
-            <label
-              htmlFor="pw"
-              style={{ fontSize: "12px" }}
-              className="label"
-            ></label>
-            <input
-              type="text"
-              placeholder="비밀번호"
-              id="pw"
-              value={password}
-              onChange={handleInputChange}
-            />
-            <label
-              style={{
-                color: "red",
-                marginTop: "2px",
-                marginLeft: "8px",
-                fontSize: "12px",
-              }}
-            >
-              {!isPasswordRule
-                ? "알파벳, 숫자, 특수문자가 1개 이상 들어가야 합니다."
-                : ""}
-            </label>
-          </p>
-          <p>
-            <label
-              htmlFor="pw-check"
-              style={{ fontSize: "12px" }}
-              className="label"
-            ></label>
-            <input
-              type="text"
-              placeholder="비밀번호 확인"
-              id="pw-check"
-              value={passwordCheck}
-              onChange={handleInputChange}
-            />
-            <label
-              style={{
-                color: "red",
-                marginTop: "2px",
-                marginLeft: "8px",
-                fontSize: "12px",
-              }}
-            >
-              {!isPasswordMatch ? "비밀번호가 같지 않습니다." : ""}
-            </label>
-          </p>
-          <p>
-            <button
-              className="submit-button"
-              disabled={isButtonDisabled}
-              onClick={handleSubmit}
-            >
-              가입하기
-            </button>
-          </p>
+        <div className="box-title">
+          <h1>회원가입 페이지</h1>
+        </div>
+        <hr />
+        <form method="POST" name="SignUp" onSubmit={submitHandler}>
+          <input
+            placeholder="이름"
+            onChange={(event) => setName(event.target.value)}
+          ></input>
+          {nameMessage && <span>{nameMessage}</span>}
+          <input
+            placeholder="이메일"
+            onChange={(event) => setEmail(event.target.value)}
+          ></input>
+          {emailMessage && <span>{emailMessage}</span>}
+          <input
+            placeholder="나이"
+            onChange={(event) => setAge(event.target.value)}
+          ></input>
+          {ageMessage && <span>{ageMessage}</span>}
+          <input
+            placeholder="비밀번호"
+            type="password"
+            onChange={(event) => setPassword(event.target.value)}
+          ></input>
+          {passwordMessage && <span>{passwordMessage}</span>}
+          <input
+            placeholder="비밀번호 확인"
+            type="password"
+            onChange={(event) => setCheckPassword(event.target.value)}
+          ></input>
+          {checkPasswordMessage && <span>{checkPasswordMessage}</span>}
+          <button type="submit">제출하기</button>
         </form>
       </div>
     </S.Container>
   );
 }
-
-export default SignUp;
