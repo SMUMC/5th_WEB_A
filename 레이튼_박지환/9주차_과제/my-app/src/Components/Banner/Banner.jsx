058ff826204f815ed73575dc41b19d237938522f
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2023-11-25 16:37:48 +0900

    chore:login logic 수정

--- 레이튼_박지환/9주차_과제/my-app/src/Components/Banner/Banner.jsx
+++ 레이튼_박지환/9주차_과제/my-app/src/Components/Banner/Banner.jsx
@@ -1,10 +1,12 @@
 import * as S from "../Banner/Banner.style";
 import axios from "axios";
 import { useEffect, useState } from "react";
-
+import { useSelector } from "react-redux/es/hooks/useSelector";
 export default function Banner() {
   const [data, setData] = useState(false);
-  const [isLoading, setIsLoading] = useState(true);
+  const isPending = useSelector((state) => state.signUp.isPending);
+  const Login = useSelector((state) => state.signUp.isLogin); // 로그아웃시 state변화를 만들기 위해서 로그인 상태를 검증한다.
+
   useEffect(() => {
     const token = localStorage.getItem("token");
 
@@ -20,28 +22,24 @@ export default function Banner() {
       try {
         const response = await axios(options);
         setData(response.data.username);
-        setIsLoading(false);
       } catch (error) {
         console.error(error);
-        setIsLoading(false);
       }
     };
 
     if (token) {
       fetchUserData();
-    } else {
-      setIsLoading(false);
     }
-  }, [isLoading]);
+  }, [isPending]);
   console.log(data);
   return (
     <S.BannerContainer>
-      {isLoading ? (
+      {isPending ? (
         <h2>로딩중 입니다!</h2>
       ) : (
         <>
           <h1>Break The Rules</h1>
-          {data ? <h2>{data}님 환영합니다!</h2> : <h2>환영합니다.</h2>}
+          {Login ? <h2>{data}님 환영합니다!</h2> : <h2>환영합니다.</h2>}
           <h3>기말고사 파이팅!</h3>
         </>
       )}
