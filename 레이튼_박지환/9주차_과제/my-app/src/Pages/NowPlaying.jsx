12d62d14a31873fe888e4caa7e6c9c2fddbc9f8a
park jihwan <123473343+zihwanpack@users.noreply.github.com>
2023-11-25 16:03:03 +0900

    chore:throttling 구현

--- 레이튼_박지환/9주차_과제/my-app/src/Pages/NowPlaying.jsx
+++ 레이튼_박지환/9주차_과제/my-app/src/Pages/NowPlaying.jsx
@@ -1,8 +1,9 @@
 import { FetchNowPlaying } from "../Hooks/FetchNowPlaying";
 import MovieList from "../Components/MovieList/MovieList";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function NowPlaying() {
+  const [throttle, setThrottle] = useState(false);
   const { isData, currentPage, changePage, isLoading } =
     FetchNowPlaying("now_playing");
 
@@ -12,8 +13,17 @@ export default function NowPlaying() {
         window.innerHeight + window.scrollY >=
         document.body.scrollHeight - 200
       ) {
-        changePage(currentPage + 1);
-        console.log("API 요청");
+        if (throttle) return; // throttle이 true면 무시
+        if (!throttle) {
+          // throttle false일 때
+          setThrottle(true); // throttle true로 바꾼다.
+          setTimeout(async () => {
+            // 0.5초뒤 콜백함수 실행
+            changePage(currentPage + 1); // 페이지 하나 더 불러오기
+            console.log("API 요청");
+            setThrottle(false);
+          }, 500);
+        }
       }
     };
     window.addEventListener("scroll", handleScroll);
